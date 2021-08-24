6c7c0428ecc5fe5ae2b0d35bc386c8d9bf2d85b2
Jônatas Senna <53903172+DevJonh@users.noreply.github.com>
2021-08-23 22:10:45 -0300

    style: creation of the global theme of the app

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,17 @@
+export default {
+  colors: {
+    primary: "#5636d3",
+    secondary: "#FF872c",
+    secondary_ligth: "rgba(255,135,44,.3)",
+
+    success: "#12a454",
+    success_ligth: "rgba(18,164,84,.5)",
+    attention: "#e83f5b",
+    attention_ligth: "rgba(232,63,91,.5)",
+
+    shape: "#ffffff",
+    title: "#363f5f",
+    text: "#969cb2",
+    background: "#f0f2f5",
+  },
+};
